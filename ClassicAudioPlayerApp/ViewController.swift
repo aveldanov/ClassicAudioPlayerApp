be2b8681ecc09d383cef9bc6f75dc5130207cca5
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-01 14:34:08 -0700

    first commit

---
+++ ClassicAudioPlayerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassicAudioPlayerApp
+//
+//  Created by Veldanov, Anton on 5/1/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
