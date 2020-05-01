212e72f52b04478c2c95e62d464288f6c7a00f33
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-01 15:07:31 -0700

    iOS

--- ClassicAudioPlayerApp/ViewController.swift
+++ ClassicAudioPlayerApp/ViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+  var player = AVAudioPlayer()
+  var timer = Timer()
+     let audioPath = Bundle.main.path(forResource: "mozart", ofType: "mp3")
+  
+  
+  
+  @IBOutlet weak var volumeOutlet: UISlider!
+  @IBOutlet weak var positionOutlet: UISlider!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
 
+ 
+    
+    do{
+      try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+      
+      positionOutlet.maximumValue = Float(player.duration)
+      
+      
+      
+    }catch{
+      
+      
+      
+    }
+
+  }
+  
+  
+  @IBAction func pauseButtonPressed(_ sender: UIBarButtonItem) {
+    player.pause()
+    timer.invalidate()
+  }
+  
+  @IBAction func playButtonPressed(_ sender: UIBarButtonItem) {
+    player.play()
+    
+    timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(updateSliderPosition), userInfo: nil, repeats: true)
+    
+  }
+  @IBAction func stopButtonPressed(_ sender: UIBarButtonItem) {
+    player.pause()
+    timer.invalidate()
+
+    // resetting the player
+    do{
+      try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+    }catch{
+
+    }
+  }
+  
+  
+  
+  
+  @IBAction func possitionTapped(_ sender: UISlider) {
+    
+    
+    player.currentTime = TimeInterval(positionOutlet.value)
+    
+    
+  }
+  
+  
+  
+  @IBAction func volumeTapped(_ sender: UISlider) {
+    player.volume = volumeOutlet.value
+  }
+  
+  
+  @objc func updateSliderPosition(){
+    
+    positionOutlet.value = Float(player.currentTime)
+  }
+  
 
 }
 
